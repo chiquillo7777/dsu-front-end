f70293fe8acd8968c95b55ed669d83fce04d425b
carlos chiquillo <carlosjosechiquillolopez@gmail.com>
2020-03-16 14:21:41 -0600

    moving html content to react component, jsx

--- marvel-project/src/App.js
+++ marvel-project/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Body } from './components/Body'
 
 function App() {
   return (
     <div className="App">
-     
+      <Body />
     </div>
   );
 }
---
+++ marvel-project/src/components/Body.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+export const Body = () => {
+    return (
+        <div>
+            <div class="slidershow middle">
+                <div class="slides">
+                    <input type="radio" name="r" id="r1" checked />
+                    <input type="radio" name="r" id="r2" />
+                    <input type="radio" name="r" id="r3" />
+                    <input type="radio" name="r" id="r4" />
+                    <input type="radio" name="r" id="r5" />
+                    <div class="slide">
+                        <img src="wolverine.jpg" alt="Wolverine" />
+                    </div>
+                    <div class="slide s1">
+                        <img src="spiderman.jpg" alt="Spiderman" />
+                    </div>
+                    <div class="slide">
+                        <img src="ironman.jpg" alt="Iron man" />
+                    </div>
+                    <div class="slide">
+                        <img src="black-panther.jpg" alt="Black Panther" />
+                    </div>
+                    <div class="slide">
+                        <img src="black-widow.jpg" alt="Black Widow" />
+                    </div>
+                </div>
+                <div class="navigation">
+                    <label for="r1" class="bar"></label>
+                    <label for="r2" class="bar"></label>
+                    <label for="r3" class="bar"></label>
+                    <label for="r4" class="bar"></label>
+                    <label for="r5" class="bar"></label>
+                </div>
+
+            </div>
+
+            <div class="search-box">
+                <input class="search-txt" type="text" name="" placeholder="Type to search" />
+                <a class="search-btn" href="#">
+                    <i class="fas fa-search"></i>
+                </a>
+            </div>
+
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+
+
+
+            
+        </div>
+    )
+}
+
